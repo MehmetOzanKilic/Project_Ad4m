653f30966b99e0dea6782872bfd033fe87fff243
MehmetOzanKilic <kilic.mehmetozan1@gmail.com>
2024-02-23 20:28:43 +0300

    23.02.2024 ikinci

--- Ad4m's Game/Assets/Scripts/Bullet.cs
+++ Ad4m's Game/Assets/Scripts/Bullet.cs
@@ -8,6 +8,8 @@ public class Bullet : MonoBehaviour
     [HideInInspector] public Vector3 move;
     [SerializeField] private float speed;
 
+    private float time=0.0f;
+
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -19,11 +21,16 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(time>=10)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition((Vector3) transform.position + (move * speed * Time.deltaTime));
+
+        time = time + 0.1f;
     }
 }
