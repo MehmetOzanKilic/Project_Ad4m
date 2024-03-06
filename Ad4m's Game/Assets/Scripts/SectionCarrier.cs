593e87f2dbff2e6a26b433412c40466282853ac8
MehmetOzanKilic <kilic.mehmetozan1@gmail.com>
2024-03-07 01:40:35 +0300

    07.03.2024 SnapController Computer
    Sections can be merged together on the computer screen.

--- Ad4m's Game/Assets/Scripts/SectionCarrier.cs
+++ Ad4m's Game/Assets/Scripts/SectionCarrier.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class SectionCarrier : MonoBehaviour
 {
-    [SerializeField]private Vector3 offset;
+    //makes it easier to call functions in between scripts
+    public delegate void DragEndedDelegate(SectionCarrier draggableObject);
+    public DragEndedDelegate dragEndedCallBack;
+
+    //how far the mouse is from the center of the section
+    private Vector3 offset;
+
     private Vector3 startPos;
 
     private bool dragging=false;
 
-    private bool inPlace=false;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        //Call the find function a little bit later to ensure everything is in place first
         Invoke("find",0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //updates the posiion of the section according to the mouse position
         if(dragging)
         {
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
@@ -27,24 +34,22 @@ public class SectionCarrier : MonoBehaviour
 
     void OnMouseDown()
     {
-        Debug.Log("pressed");
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         dragging = true;
     }
 
     void OnMouseUp()
     {
-        Debug.Log("let go");
         dragging = false;
-        if(!inPlace)
-        {
-            transform.position = startPos;
-        }
+        //send the section back to the original position when the mouse button is up
+        transform.position = startPos;
+        //will send this object and script to the snapController to check if the section will snap or not
+        dragEndedCallBack(this);
     }
 
     private void find()
     {
+        //seperate function to designate starting positions after some time
         startPos= transform.position;
-        Debug.Log(startPos);
     }
 }
